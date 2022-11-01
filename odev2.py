90e3fd1fac6c3bbca6ec7854687243fac4924df9
erdgn34 <116517667+erdgn34@users.noreply.github.com>
2022-11-01 23:41:14 +0300

    Add files via upload

---
+++ odev2.py
@@ -0,0 +1,25 @@
+
+
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+resim=cv2.imread("kedi.jpg",0)
+uzunluk=resim.shape[0]
+genislik=resim.shape[1]
+max=0
+for i in range(uzunluk):
+    for j in range(genislik):
+        if resim[i][j]>max:
+            max=resim[i][j]
+
+ters_resim=max-resim
+cv2.imshow("kedi",ters_resim)
+cv2.waitKey(0)
+
+
+
+
+
+
